d8f7f670b052619e0f71145fe17c2257e28929ed
Wisdom Tochi <agbasionyewisdom@gmail.com>
2024-01-14 18:22:12 -0800

    TestingKwipikAPI

--- TestingKwikpikAPI/DTO/CreateDispatchRequest/CreateDispatchRequestDTO.cs
+++ TestingKwikpikAPI/DTO/CreateDispatchRequest/CreateDispatchRequestDTO.cs
@@ -3,22 +3,54 @@
 
     public class CreateDispatchRequestDTO
     {
-        public string pickupAddress { get; set; }
-        public string destinationAddress { get; set; }
-        public string vehicleType { get; set; }
+        public Result result { get; set; }
+
+        public static implicit operator CreateDispatchRequestDTO(object v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Result
+    {
+        public Data data { get; set; }
+    }
+
+    public class Data
+    {
+        public Location location { get; set; }
+        public Packagedetails packageDetails { get; set; }
+        public string selectedVehicleType { get; set; }
         public string userType { get; set; }
-        public string recipientName { get; set; }
+        public Destination destination { get; set; }
         public string recipientPhoneNumber { get; set; }
+        public string recipientName { get; set; }
         public string phoneNumber { get; set; }
+        public string senderName { get; set; }
+        public string currency { get; set; }
+    }
+
+    public class Location
+    {
+        public float latitude { get; set; }
+        public float longitude { get; set; }
+    }
+
+    public class Packagedetails
+    {
         public string category { get; set; }
         public string product { get; set; }
         public string description { get; set; }
-        public string weight { get; set; }
+        public int weight { get; set; }
         public int quantity { get; set; }
-        public string packageValue { get; set; }
         public string image { get; set; }
-        public string currency { get; set; }
-        public string senderName { get; set; }
+        public object value { get; set; }
+    }
+
+    public class Destination
+    {
+        public float latitude { get; set; }
+        public float longitude { get; set; }
     }
 
 }
--- TestingKwikpikAPI/Data Access/CreateDispatchRequestRepo/CreateDispatchRequestAPI.cs
+++ TestingKwikpikAPI/Data Access/CreateDispatchRequestRepo/CreateDispatchRequestAPI.cs
@@ -13,16 +13,23 @@ namespace TestingKwikpikAPI.Data_Access.CreateDispatchRequestRepo
             this.httpClient = httpClient;
         }
 
-        public async Task<Enum> CreateDispatchRequest(CreateDispatchRequestDTO createDispatchRequestDTO)
+        public async Task<Enum> CreateDispatchRequest(CreateDispatchRequestDTO dispatchRequestDTO)
         {
             try
             {
-                httpClient.BaseAddress = new Uri("https://dev-gateway.kwikpik.io/api/");
+                var payLoad = new { key = "fGKa77uvaeROvwh1MXR7X63VOtWXIomjBKQDbacI31EEafOD49Er4HXuALsc" };
+
+                var baseUri = new Uri("https://dev-gateway.kwikpik.io/api/");
+                var uri = new Uri(baseUri, "broker/init_request_ride/business");
+
+                httpClient.BaseAddress = baseUri;
                 httpClient.DefaultRequestHeaders.Accept.Clear();
                 httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                 httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("X-API-Key", "fGKa77uvaeROvwh1MXR7X63VOtWXIomjBKQDbacI31EEafOD49Er4HXuALsc");
 
-                var result = await httpClient.GetAsync("broker/init_request_ride/business");
+
+                //var stringContent = new StringContent(payLoad, UnicodeEncoding.UTF8, "application/json");
+                var result = await httpClient.PostAsJsonAsync(uri, (object)payLoad);
                 var response = await result.Content.ReadAsStringAsync();
                 return Enums.DispatchRequestCreated;
             }
